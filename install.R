4d2ebe46137d08e2c019a8c91c76a60f41aa27c7
rsm-ssumantmysore <ssumantmysore@ucsd.edu>
2025-06-09 10:25:48 -0700

    1

--- install.R
+++ install.R
@@ -1,2 +1,9 @@
-install.packages("gridExtra")
+# Install if necessary
+if (!require(tidyverse)) install.packages("tidyverse")
+
+# Load tidyverse (which includes dplyr, readr, ggplot2, etc.)
+library(tidyverse)
+
+# Optional: for layout of multiple ggplots
+if (!require(gridExtra)) install.packages("gridExtra")
 library(gridExtra)
